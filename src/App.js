0b728ee81f3e2670532a3eb54d464b7ab239dead
debdut.saha <debdut.saha@angelbroking.com>
2023-09-24 15:39:02 +0530

    moved the code to add module

--- src/App.js
+++ src/App.js
@@ -1,1 +1,40 @@
-try {console.log(require('@react-native-community/cli').bin);} catch (e) {console.log(require('react-native/cli').bin);}
+import React, { useState } from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+
+const App = () => {
+    const [count, setCount] = useState(0);
+    const onPress = () => setCount(prevCount => prevCount + 1);
+  
+    return (
+      <View style={styles.container}>
+        <Text style={styles.hello}>{count}</Text>
+        <TouchableOpacity style={styles.button} onPress={onPress} activeOpacity={0.7}>
+          <Text>Click Me</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+  const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      backgroundColor: "cyan",
+      alignContent:"center"
+    },
+    hello: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+    button: {
+      width: 100,
+      height: 40,
+      backgroundColor: "red",
+      justifyContent: "center",
+      borderRadius: 4,
+      alignItems: "center",
+      alignSelf:"center"
+    }
+  });
+
+export default App
